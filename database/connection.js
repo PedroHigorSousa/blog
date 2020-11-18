219673ac9c923c0b9d4ced6ab3da2adf816a9925
Pedro Higor de Sousa <53284345+PedroHigorSousa@users.noreply.github.com>
2020-11-18 02:03:18 -0300

    Delete connection.js

--- database/connection.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-const connection = new Sequelize('blog', 'root', 'yg7s-ecuj', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-
-module.exports = connection;
